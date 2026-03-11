8593c0702d15da4804e6a6e5d2ea26b23bff09ba
nhattvim <nhattruong13112000@gmail.com>
2026-03-11 09:49:54 +0700

    first commit

---
+++ be/src/main/java/com/nhattVim/TravelTo/TravelToApplication.java
@@ -0,0 +1,13 @@
+package com.nhattVim.TravelTo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelToApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/nhattVim/TravelTo/TravelToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhattVim.TravelTo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
